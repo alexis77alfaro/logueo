5eb267eb8e9f559c0a3d4f74ae8461ab70eb5c5e
alexis77alfaro <47479100+alexis77alfaro@users.noreply.github.com>
2020-05-11 16:07:20 -0600

    finzalizado

--- src/main/java/com/logue/Dao/HistorialDao.java
+++ src/main/java/com/logue/Dao/HistorialDao.java
@@ -9,11 +9,13 @@ import javax.persistence.Persistence;
 
 
 import com.logueo.model.TbHistorial;
+import com.logueo.model.TbUsuariop;
 
 
 
 public class HistorialDao {
-	
+	TbUsuariop usu=new TbUsuariop();
+	TbHistorial usa= new TbHistorial();
 	
 	public void agregarhistorial(TbHistorial his) {
 		
@@ -44,17 +46,16 @@ public class HistorialDao {
 		
 		try {
 			em.getTransaction().begin();
-			lista=em.createQuery("SELECT his.idHistorial, his.tbUsuariop.idUsuarios,his.fechas,"
-					+ "usu.nombre_usuario,usu.apellido_usuario"
-					+ "FROM TbHistorial AS his "
-					+ "INNER JOIN TbUsuariop AS usu ON usu.idUsuarios= his.tbUsuariop.idUsuarios").getResultList();
+			lista=em.createQuery("SELECT his.idHistorial, his.tbUsuariop.idUsuarios, his.fecha,usu.nombre_usuario, usu.apellido_usuario FROM TbHistorial AS his INNER JOIN TbUsuariop AS usu ON usu.idUsuarios = his.tbUsuariop.idUsuarios").getResultList();
 			em.getTransaction().commit();
-			
+			  
 			
 		}catch(Exception e) {
 			System.out.println(e);
 		}
 		return lista;
+		
+		
 		
 	}
 	
